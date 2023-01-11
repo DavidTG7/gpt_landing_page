cc57bf811553406558c73839da15223bfcd11c9b
DavidTG7 <davidthekoa@gmail.com>
2023-01-11 00:38:21 -0500

    Initial commit, setting project up, create folder structure and all components

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./brand.css"
+
+export const Brand = () => {
+  return <div>Brand</div>;
+};
---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./features.css";
+
+export const Features = () => {
+  return <div>Features</div>;
+};
