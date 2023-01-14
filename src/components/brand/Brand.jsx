b2fefcaa769fa20dcf0fc1f04643857d12ac5c27
DavidTG7 <davidthekoa@gmail.com>
2023-01-14 18:43:58 -0500

    Full code for Brand Component and its style

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
-import "./brand.css"
+import "./brand.css";
 
 export const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand">
+      <img src={google} alt="google" />
+      <img src={slack} alt="slack" />
+      <img src={atlassian} alt="altassian" />
+      <img src={dropbox} alt="dropbox" />
+      <img src={shopify} alt="shopify" />
+    </div>
+  );
 };
