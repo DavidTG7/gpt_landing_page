08854b3123a2b473ad2dbeff77b9379fa4feba1e
DavidTG7 <davidthekoa@gmail.com>
2023-01-14 18:14:04 -0500

    Complete code for Header Component

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -11,12 +11,12 @@ export const Header = () => {
         <h1 className="gradient__text">
           Let’s Build Something amazing with GPT-3 OpenAI
         </h1>
-          <p>
-            Yet bed any for travelling assistance indulgence unpleasing. Not
-            thoughts all exercise blessing. Indulgence way everything joy
-            alteration boisterous the attachment. Party we years to order allow
-            asked of.
-          </p>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
         <div className="gpt3__header-content__input">
           <input type="email" placeholder="Your Email Address" />
           <button type="button">Get Started</button>
@@ -25,9 +25,9 @@ export const Header = () => {
           <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        {/* <div className="gpt3__header-image">
-          <img src={ai} alt="image" />
-        </div> */}
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="image" />
       </div>
     </div>
   );
